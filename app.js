32ada874423822e01ffa9b1ef96add458fe29b08
wkdus0608 <seongmin020608@gmail.com>
2024-08-19 15:45:10 +0900

    색상 변경 기능 구현

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("width-range");
+const color = document.getElementById("color");
+
 
 canvas.width = 800;
 canvas.height = 800;
@@ -25,6 +27,11 @@ function onLineWidthChange(event) {
     ctx.beginPath();
     ctx.lineWidth = event.target.value;
 }
+function onColorChange(event) {
+    ctx.beginPath();
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
 
 
 canvas.addEventListener("mousemove", onMove);
@@ -33,3 +40,5 @@ canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+
+color.addEventListener("change", onColorChange);
