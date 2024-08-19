b5338dc44f08f1297e13f5a40e36b92842166edb
wkdus0608 <seongmin020608@gmail.com>
2024-08-19 14:36:31 +0900

    (0,0)에서 클릭한 위치까지 랜덤한 색상의 선을 그음

--- app.js
+++ app.js
@@ -4,10 +4,38 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50,50);
-ctx.lineTo(100,50);
-ctx.lineTo(100,100);
-ctx.lineTo(50,100);
-ctx.lineTo(50,50);
 
-ctx.fill();
+//클릭하면 event.offsetX event.offsetY로 선 그림
+
+const colors = [
+    // "#1abc9c",
+    // "#2ecc71",
+    // "#3498db",
+    // "#9b59b6",
+    // "#34495e",
+    // "#16a085",
+    // "#27ae60",
+    // "#2980b9",
+    // "#8e44ad",
+    // "#2c3e50",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+];
+
+
+
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("click", onClick);
