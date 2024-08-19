e530051bf74f54ef50a0528c736a40eb34f9b41b
wkdus0608 <seongmin020608@gmail.com>
2024-08-19 14:57:47 +0900

    마우스 누른상태일 때 그리기. 마우스 때면 중지. 캔버스 나가도 중지

--- app.js
+++ app.js
@@ -4,38 +4,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+let isPainting = false;
 
-//클릭하면 event.offsetX event.offsetY로 선 그림
-
-const colors = [
-    // "#1abc9c",
-    // "#2ecc71",
-    // "#3498db",
-    // "#9b59b6",
-    // "#34495e",
-    // "#16a085",
-    // "#27ae60",
-    // "#2980b9",
-    // "#8e44ad",
-    // "#2c3e50",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "violet",
-];
-
-
-
-function onClick(event) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-
-    ctx.strokeStyle = color;
+function onMove(event) {
+    if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
+    // return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("click", onClick);
+
+function onMouseDown() {
+    isPainting = true;
+}
+function stopPainting() {
+    isPainting = false;
+}
+
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting);
